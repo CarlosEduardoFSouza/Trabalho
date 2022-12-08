7f9a000de77d5cdce58b3708abe3bbf179ddb79a
Carlos Eduardo F Souza <carloseduardofernandes10@live.com>
2022-12-07 22:47:18 -0300

    More atualizations

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'curso-angular'`, () => {
+  it(`should have as title 'Trabalho-Angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('curso-angular');
+    expect(app.title).toEqual('Trabalho-Angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('curso-angular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Trabalho-Angular app is running!');
   });
 });
